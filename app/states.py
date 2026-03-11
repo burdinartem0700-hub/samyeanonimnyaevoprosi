f18eb42ad3ea5366fb4a537ddefa41e211ef3583
burdinartem0700-hub <burdinartem0700@gmail.com>
2026-03-11 22:34:40 +0400

    Add files via upload

--- app/states.py
+++ app/states.py
@@ -2,3 +2,17 @@ from aiogram.fsm.state import State, StatesGroup
 
 class idea(StatesGroup):
     idea_text = State()
+
+class Premium(StatesGroup):
+    add_premka = State()
+    del_premka = State()
+    id_state = State()
+    first_state = State()
+    username_state = State()
+
+class StaticState(StatesGroup):
+    userid = State()
+    referid = State()
+    send = State()
+    rec = State()
+    refer = State()
