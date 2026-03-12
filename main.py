c66e3affe7491805396f65b0e4d5037bdac42f0d
burdinartem0700-hub <burdinartem0700@gmail.com>
2026-03-12 16:43:59 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import asyncio
 import json
 import os
 
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from aiogram import Bot, Dispatcher, F, Router
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import CommandStart, Command, CommandObject
 from app.heandlers import router
 from app.DARABASE import init_db
+from aiogram.client.default import DefaultBotProperties
 
 back_send = InlineKeyboardMarkup(inline_keyboard =[
     [InlineKeyboardButton(text = "Отменить", callback_data="back_sen")]
@@ -14,8 +16,7 @@ back_send = InlineKeyboardMarkup(inline_keyboard =[
 
 async def main():
     bot = Bot(token='8569542955:AAHz8MZHouGM8Vy52Aa5_47iFvWXIXh-U1I')
-    dp = Dispatcher(storage=MemoryStorage())
-    init_db()
+    dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
@@ -23,4 +24,5 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
+
         print("Exit")
